f51d80b337985bc1fa9206edc1a290a565967688
Alexander Zaytsev <alexander.zaytsev@doneright.ru>
2012-04-18 12:37:16 +0600

    Update ForeignKeyConstraintNameConvention - generate constraints for joined-subclasses

--- src/NHibernate/Conventions/NameConventions.cs
+++ src/NHibernate/Conventions/NameConventions.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Text.RegularExpressions;
-
-namespace Brandy.NHibernate.Conventions
+﻿namespace Brandy.NHibernate.Conventions
 {
+    using System;
+    using System.Text.RegularExpressions;
+
     public static class NameConventions
-	{
-		public static string GetTableName(Type type)
-		{
-			return ReplaceCamelCaseWithUnderscore(type.Name);
-		}
+    {
+        public static string GetTableName(Type type)
+        {
+            return ReplaceCamelCaseWithUnderscore(type.Name);
+        }
 
-		internal static string ReplaceCamelCaseWithUnderscore(string name)
-		{
-			return Regex.Replace(name, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1_").ToUpper();
-		}
+        public static string GetSequenceName(Type type)
+        {
+            return String.Format("{0}_SEQ", GetTableName(type));
+        }
 
-		public static string GetSequenceName(Type type)
-		{
-			return string.Format("{0}_SEQ", GetTableName(type));
-		}
+        public static string GetPrimaryKeyColumnName(Type type)
+        {
+            return String.Format("{0}_ID", GetTableName(type));
+        }
 
-		public static string GetPrimaryKeyColumnName(Type type)
-		{
-			return string.Format("{0}_ID", GetTableName(type));
-		}
-	}
+        public static string ReplaceCamelCaseWithUnderscore(string name)
+        {
+            return Regex.Replace(name, "([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))", "$1_").ToUpper();
+        }
+
+        public static string GetForeignKeyConstraintName(string child, string parent)
+        {
+            return String.Format("FK_{0}_{1}", ReplaceCamelCaseWithUnderscore(child), ReplaceCamelCaseWithUnderscore(parent)).ToUpper();
+        }
+    }
 }
+
